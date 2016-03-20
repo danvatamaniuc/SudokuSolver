c219ab1f35046cad8bc3ee615c4d97e984c70985
danvatamaniuc <da.vatamaniuc@gmail.com>
2016-03-20 22:35:37 +0200

    Initial Upload
    Base files of the Sudoku Solver app.

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace SudokuSolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSize = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.dgvSudokuTable = new System.Windows.Forms.DataGridView();
+            this.cmdGenerateSize = new System.Windows.Forms.Button();
+            this.cmdSolve = new System.Windows.Forms.Button();
+            this.rbBFS = new System.Windows.Forms.RadioButton();
+            this.rbGBFS = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSudokuTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(13, 13);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(84, 13);
+            this.lblSize.TabIndex = 0;
+            this.lblSize.Text = "Marimea tabelei:";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(103, 10);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 1;
+            // 
+            // dgvSudokuTable
+            // 
+            this.dgvSudokuTable.AllowUserToAddRows = false;
+            this.dgvSudokuTable.AllowUserToDeleteRows = false;
+            this.dgvSudokuTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSudokuTable.Location = new System.Drawing.Point(16, 36);
+            this.dgvSudokuTable.Name = "dgvSudokuTable";
+            this.dgvSudokuTable.RowHeadersWidth = 18;
+            this.dgvSudokuTable.Size = new System.Drawing.Size(478, 304);
+            this.dgvSudokuTable.TabIndex = 2;
+            // 
+            // cmdGenerateSize
+            // 
+            this.cmdGenerateSize.Location = new System.Drawing.Point(210, 10);
+            this.cmdGenerateSize.Name = "cmdGenerateSize";
+            this.cmdGenerateSize.Size = new System.Drawing.Size(75, 20);
+            this.cmdGenerateSize.TabIndex = 3;
+            this.cmdGenerateSize.Text = "Generate";
+            this.cmdGenerateSize.UseVisualStyleBackColor = true;
+            this.cmdGenerateSize.Click += new System.EventHandler(this.cmdGenerateSize_Click);
+            // 
+            // cmdSolve
+            // 
+            this.cmdSolve.Location = new System.Drawing.Point(419, 8);
+            this.cmdSolve.Name = "cmdSolve";
+            this.cmdSolve.Size = new System.Drawing.Size(75, 23);
+            this.cmdSolve.TabIndex = 4;
+            this.cmdSolve.Text = "Solve";
+            this.cmdSolve.UseVisualStyleBackColor = true;
+            this.cmdSolve.Click += new System.EventHandler(this.cmdSolve_Click);
+            // 
+            // rbBFS
+            // 
+            this.rbBFS.AutoSize = true;
+            this.rbBFS.Location = new System.Drawing.Point(300, 11);
+            this.rbBFS.Name = "rbBFS";
+            this.rbBFS.Size = new System.Drawing.Size(45, 17);
+            this.rbBFS.TabIndex = 5;
+            this.rbBFS.TabStop = true;
+            this.rbBFS.Text = "BFS";
+            this.rbBFS.UseVisualStyleBackColor = true;
+            // 
+            // rbGBFS
+            // 
+            this.rbGBFS.AutoSize = true;
+            this.rbGBFS.Location = new System.Drawing.Point(351, 11);
+            this.rbGBFS.Name = "rbGBFS";
+            this.rbGBFS.Size = new System.Drawing.Size(53, 17);
+            this.rbGBFS.TabIndex = 6;
+            this.rbGBFS.TabStop = true;
+            this.rbGBFS.Text = "GBFS";
+            this.rbGBFS.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 352);
+            this.Controls.Add(this.rbGBFS);
+            this.Controls.Add(this.rbBFS);
+            this.Controls.Add(this.cmdSolve);
+            this.Controls.Add(this.cmdGenerateSize);
+            this.Controls.Add(this.dgvSudokuTable);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.lblSize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSudokuTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.DataGridView dgvSudokuTable;
+        private System.Windows.Forms.Button cmdGenerateSize;
+        private System.Windows.Forms.Button cmdSolve;
+        private System.Windows.Forms.RadioButton rbBFS;
+        private System.Windows.Forms.RadioButton rbGBFS;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+using SudokuSolver.Logic;
+using SudokuSolver.Validators;
+
+namespace SudokuSolver
+{
+    public partial class Form1 : Form
+    {
+        private int _size;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdGenerateSize_Click(object sender, EventArgs e)
+        {
+
+            _size = -1;
+
+            //validate the user input
+            try
+            {
+                Int32.TryParse(txtSize.Text, out _size);
+                Validator.ValidateGridSize(_size);
+            }
+            catch (ValidationException v)
+            {
+                MessageBox.Show(v.Message);
+                return;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something unexpected happened!");
+                return;
+            }
+
+            dgvSudokuTable.Columns.Clear();
+
+            for (int i = 0; i < _size; i++)
+            {
+                dgvSudokuTable.Columns.Add((i+1).ToString(), (i+1).ToString());
+                dgvSudokuTable.Columns[i].Width = 20;
+                dgvSudokuTable.Rows.Add();
+            }
+        }
+
+        private void cmdSolve_Click(object sender, EventArgs e)
+        {
+            int currentNumber = -1;
+
+            int[,] sudokuTable = new int[_size,_size];
+
+            //extract the table from the datagridview
+            for (int rows = 0; rows < dgvSudokuTable.Rows.Count; rows++)
+            {
+                for (int col = 0; col < dgvSudokuTable.Rows[rows].Cells.Count; col++)
+                {
+
+                    if (dgvSudokuTable.Rows[rows].Cells[col].Value == null)
+                    {
+                        sudokuTable[rows, col] = -1;
+                    }
+                    else
+                    {
+                        string value = dgvSudokuTable.Rows[rows].Cells[col].Value.ToString();
+
+                        try
+                        {
+                            currentNumber = int.Parse(value);
+
+                            if (currentNumber > _size)
+                            {
+                                throw new ValidationException("Sudoku table not valid! Allowed values are from 1 to " +
+                                                              _size);
+                            }
+
+                        }
+                        catch (ValidationException v)
+                        {
+                            MessageBox.Show(v.Message);
+                            return;
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show(
+                                "Something happened. Most likely you inserted a character instead of a number somewhere");
+                            return;
+                        }
+
+                        sudokuTable[rows, col] = currentNumber;
+                    }
+
+                }
+            }
+            //end extraction of values from data grid view
+
+            try
+            {
+                Validator.ValidateSudokuTable(sudokuTable);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+                return;
+            }
+
+            int[,] solution;
+
+            if (rbBFS.Checked)
+            {
+                solution = Solver.FindSolution(sudokuTable);
+            }
+            else
+            {
+                solution = Solver.FindSolutionGBFS(sudokuTable);
+            }
+
+
+            if (solution == null)
+            {
+                MessageBox.Show("A solution has not been found!");
+                return;
+            }
+
+            for (int rows = 0; rows < dgvSudokuTable.Rows.Count; rows++)
+            {
+                for (int col = 0; col < dgvSudokuTable.Rows[rows].Cells.Count; col++)
+                {
+                    dgvSudokuTable.Rows[rows].Cells[col].Value = solution[rows, col];
+                }
+            }
+        }
+    }
+}
